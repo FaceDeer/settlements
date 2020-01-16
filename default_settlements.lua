be02dcfade2abd7a0553b70b3df12c43f74f69c7
FaceDeer <derksenmobile@gmail.com>
2020-01-15 20:02:10 -0700

    Put some wretched ruins out on the tundra, tundra needs a little love

--- default_settlements.lua
+++ default_settlements.lua
@@ -333,3 +333,68 @@ end
 settlements.register_settlement("desert", desert_settlements)
 
 end
+
+if minetest.settings:get_bool("settlements_ruins", true) and modpath_default and modpath_stairs then
+
+local ruin_settlements = {
+	surface_materials = {
+		"default:permafrost",
+		"default:permafrost_with_stones",
+		"default:permafrost_with_moss",
+	},
+
+	platform_shallow = "default:gravel",
+	platform_deep = "default:stone",
+	
+	building_count_min = 3,
+	building_count_max = 15,
+
+	altitude_min = 2,
+	altitude_max = 300,
+	
+	schematics = {
+		{
+			name = "ruin_small",
+			schematic = dofile(schem_path.."ruin_small.lua"),
+			buffer = 1,
+			max_num = 0.7,
+			platform_clear_above = false,
+		},
+		{
+			name = "ruin_med",
+			schematic = dofile(schem_path.."ruin_med.lua"),
+			buffer = 1,
+			max_num = 0.4,
+			platform_clear_above = false,
+		},
+		{
+			name = "ruin_monolith",
+			schematic = dofile(schem_path.."ruin_monolith.lua"),
+			buffer = 1,
+			max_num = 0.1,
+			platform_clear_above = false,
+		},
+		{
+			name = "ruin_tower",
+			schematic = dofile(schem_path.."ruin_tower.lua"),
+			buffer = 1,
+			max_num = 0.4,
+			platform_clear_above = false,
+		},
+	},
+	
+	generate_name = function(pos)
+		if minetest.get_modpath("namegen") then
+			return namegen.generate("ruin_settlement")
+		end	
+		return "Ruins"
+	end,
+}
+
+if minetest.get_modpath("namegen") then
+	namegen.parse_lines(io.lines(modpath.."/namegen_ruins.cfg"))
+end
+
+settlements.register_settlement("ruins", ruin_settlements)
+
+end
